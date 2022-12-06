619fd419e005066e1deffc1445bb7e2a11ee18b8
miyabitanimchi <mmyybb.mm@gmail.com>
2022-12-06 00:12:27 -0800

    83. add another solution to arrange keys

--- hashTables/hashTablePractice.js
+++ hashTables/hashTablePractice.js
@@ -50,18 +50,42 @@ class HashTable {
         const keysArray = [];
         for (let i = 0; i < this.data.length; i++) {
             if (this.data[i]) {
+                console.log('keys: ' + this.data[i]);
                 keysArray.push(this.data[i][0][0])
             }
         }
         return keysArray;
     }
+
+    keysWithCollision() {
+        if (!this.data.length) return undefined;
+
+        let result = [];
+        // loop through all the elements
+        for (let i = 0; i < this.data.length; i++) {
+            // if it's not an empty memory cell
+            if (this.data[i] && this.data[i].length) {
+                // if this.data.length > 1, that means collisions are happening somewhere so we loop through
+                if (this.data.length > 1) {
+                    for (let j = 0; j < this.data[i].length; j++) {
+                        console.log('keysWithCollision: ' + this.data[i]);
+                        result.push(this.data[i][j][0]);
+                    }
+                } else {
+                    result.push(this.data[i][0]);
+                }
+            }
+        }
+        return result;
+    }
 }
 
-const myHashTable = new HashTable(50);
+const myHashTable = new HashTable(2);
 myHashTable.set('grapes', 10000);
 myHashTable.set('apples', 10);
 myHashTable.set('pears', 200);
 // myHashTable.get('grapes');
-console.log(myHashTable.data);
-console.log(myHashTable.get('grapes'));
+// console.log(myHashTable.data);
+// console.log(myHashTable.get('grapes'));
 console.log(myHashTable.keys());
+console.log(myHashTable.keysWithCollision());
