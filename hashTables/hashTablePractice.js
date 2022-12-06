465b6dcb1e4207ce36fdfa515b0de3b1c0597ec0
miyabitanimchi <mmyybb.mm@gmail.com>
2022-12-05 23:37:22 -0800

    82. add solution of implement a hash table

--- hashTables/hashTablePractice.js
+++ hashTables/hashTablePractice.js
@@ -3,6 +3,7 @@ class HashTable {
         this.data = new Array(size);
     }
 
+    // O(1)
     // underscore means it's private in JS world... but we can still access it!
     _hash(key) {
         let hash = 0;
@@ -13,21 +14,41 @@ class HashTable {
         return hash;
     }
 
-    set(key, value) {
-        const keyAndValue = [key, value];
-        for (let i = 0; i < this.data.length; i++) {
-            this.data[i] = keyAndValue;
-        }
+    // set(key, value) {
+    //     const keyAndValue = [key, value];
+    //     for (let i = 0; i < this.data.length; i++) {
+    //         this.data[i] = keyAndValue;
+    //     }
+    //     return this.data;
+    // 
 
+    // O(1)
+    set(key, value) {
+        let address = this._hash(key);
+        if (!this.data[address]) this.data[address] = [];
+        this.data[address].push([key, value]);
         return this.data;
     }
 
+    // O(1)
     get(key) {
-
+        let address = this._hash(key);
+        const currentBucket = this.data[address];
+        // console.log(currentBucket);
+        if (currentBucket) {
+            for (let i = 0; i < currentBucket.length; i++) {
+                if (currentBucket[i][0] === key) {
+                    return currentBucket[i][1];
+                }
+            }
+        }
+        return undefined;
     }
 }
 
 const myHashTable = new HashTable(50);
-myHashTable.set('grass', 10000);
-myHashTable.get('grass');
-console.log(myHashTable);
+myHashTable.set('grapes', 10000);
+// myHashTable.set('apples', 10);
+// myHashTable.get('grapes');
+console.log(myHashTable.data);
+console.log(myHashTable.get('grapes'));
