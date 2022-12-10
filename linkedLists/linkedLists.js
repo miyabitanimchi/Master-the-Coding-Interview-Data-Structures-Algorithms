29afcd368a73aaa1c8a03555413e29ae93609f49
miyabitanimchi <mmyybb.mm@gmail.com>
2022-12-10 01:15:43 -0800

    100-102 add solution of insert

--- linkedLists/linkedLists.js
+++ linkedLists/linkedLists.js
@@ -39,6 +39,7 @@ class LinkedList {
       next: null,
     };
     this.tail.next = newNode;
+    console.log(this.head); // { value: 10, next: { value: 5, next: null } }
     this.tail = newNode;
     console.log(this.head); // { value: 10, next: { value: 5, next: null } }
     this.length++;
@@ -72,10 +73,51 @@ class LinkedList {
     this.length++;
     return this;
   }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+
+  insert(index, value) {
+    // check params
+    if (index === 0) {
+      this.prepend(value);
+      return this.printList();
+    }
+    if (index >= this.length) {
+      return this.append(value);
+    }
+
+    const newNode = new Node(value);
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList;
+  }
+
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 // myLinkedList.append(16);
 myLinkedList.prepend(1);
-console.log(myLinkedList);
+console.log(myLinkedList.printList());
+myLinkedList.insert(1, 100);
+console.log(myLinkedList.printList());
