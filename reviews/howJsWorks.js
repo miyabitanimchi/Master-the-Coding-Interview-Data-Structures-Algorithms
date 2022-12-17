1f1e04e3e12106aa1e98549e1f49f214ad0b376b
miyabitanimchi <mmyybb.mm@gmail.com>
2022-12-17 01:46:35 -0800

    117. how does js work

--- reviews/howJsWorks.js
+++ reviews/howJsWorks.js
@@ -9,17 +9,33 @@
 // - The engine consists of two parts, a memory heap and a call stack
 //   - Memory heap: Where the memory allocation happens
 //   - Call stack: where the code is read and excuted, it tells you where you are in the program
+// - ALSO, we need a JS runtime environment, it's included in browsers
+//   - web APIs
+//     setTimeout() is part of the web API, not technically part of JS
+//     Browsers give us to use so we can do asyncronous programming
+//   - Callback queue
+//   - Event loop
 
 // Memory allocation in memory heap
 const a = 1;
 const b = 10;
 const c = 100;
 
-/* Why global variables are bad */
-// It fills up memory heap and the browser will not be able to work
-
-// Cakk stacks
+// Call stacks
 console.log(1);
 console.log(2);
 console.log(3);
+// Call stack reads the first line console.log, and it gets put in the call stack
+// and it pops out from the call stack and logs the console.log, and will be removed
+
+
+/* Why global variables are bad */
+// It fills up memory heap and the browser will not be able to work
+
+
+/* what does JS is a single threaded language that can be non-blocking mean? */
+// - Single threaded means that it has only one call stack
+//   (other languages can have multiple stacks and these are called multi threaded)
+// - FILO, first in, last out
+// - Non-blocking ... there is asynchronous process with callback functions, and they gets run in the background
 
