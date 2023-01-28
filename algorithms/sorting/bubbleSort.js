d33070d3cfa489349de6d32cbc15f233c1c728a8
miyabitanimchi <mmyybb.mm@gmail.com>
2023-01-27 23:59:19 -0800

    171: correct info of bubble sort

--- algorithms/sorting/bubbleSort.js
+++ algorithms/sorting/bubbleSort.js
@@ -4,7 +4,6 @@ function bubbleSort(array) {
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length; j++) {
       if (array[j] > array[j + 1]) {
-        console.log("here");
         const temp = array[j + 1];
         array[j + 1] = array[j];
         array[j] = temp;
--- algorithms/sorting/sorting.md
+++ algorithms/sorting/sorting.md
@@ -1,4 +1,5 @@
 ### Bubble sort
 
 - One of the basic sorting algorithms
-- Not really efficient for both time and space complexity (Avarage and worst, O(n^2) of n squared)
+- Not really efficient for time complexity (Avarage and worst, O(n^2) O of n squared)
+- space complexity is O(1)
